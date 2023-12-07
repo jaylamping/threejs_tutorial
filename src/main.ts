2840b8cb119c83fbdc2998b01fd97719b81e0e45
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-12-06 23:30:03 -0600

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './style.css';
 
 const canvas = document.querySelector('.webgl') as HTMLCanvasElement;
+
+let mouseDown = false;
+let rgb = [0, 0, 0];
 
 /* scene */
 const scene = new THREE.Scene();
@@ -50,6 +54,12 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+/* timeline / initial animation */
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+tl.fromTo(mesh.scale, { x: 0, y: 0, z: 0 }, { duration: 1, x: 1, y: 1, z: 1 });
+tl.fromTo('nav', { y: '-100%' }, { duration: 1.3, y: '0%' }, '-=1');
+tl.fromTo('.title', { opacity: 0 }, { duration: 0.9, opacity: 1 }, '-=0.3');
+
 /* listeners */
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
@@ -60,6 +70,30 @@ window.addEventListener('resize', () => {
 
   renderer.setSize(sizes.width, sizes.height);
   renderer.render(scene, camera);
+});
+
+window.addEventListener('mousedown', () => {
+  mouseDown = true;
+});
+
+window.addEventListener('mouseup', () => {
+  mouseDown = false;
+});
+
+window.addEventListener('mousemove', (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    gsap.to(mesh.material.color, {
+      duration: 0.1,
+      r: rgb[0] / 255,
+      g: rgb[1] / 255,
+      b: rgb[2] / 255,
+    });
+  }
 });
 
 /* main loop */
